4263cfad79f14a865d0bc64b8aeb5cc3b5558086
Alex <narutovannassau@gmail.com>
2024-09-18 15:36:16 +0200

    first_build

--- first tower defense game/Assets/Script/Path.cs
+++ first tower defense game/Assets/Script/Path.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class Path : MonoBehaviour
 {
-    public GameObject Player;
+    public GameObject Prefab;
+
+    private float firstWave = 10;
+
+    [SerializeField] List<Transform> pathList = new List<Transform>();
+
+    private List<GameObject> Wave = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        createWave();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void createWave()
+    {
+        for (int i = 0; i < firstWave; i++)
+        {
+            GameObject newEnemy = Instantiate(Prefab) ;
+            Wave.Add(newEnemy);
+            newEnemy.GetComponent<Enemy>().targets = pathList;
+        }
     }
 }
